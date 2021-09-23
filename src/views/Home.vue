c75b9ef24e42dc82b7d4d0900eadcb9b368513eb
Likit Preeyanon <likit.pre@mahidol.edu>
2021-09-23 11:38:41 +0700

    added tabs and cards

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -9,21 +9,76 @@
     <ion-content :fullscreen="true">
       <ion-header collapse="condense">
         <ion-toolbar>
-          <ion-title size="large">Blank</ion-title>
+          <ion-title size="large">MUMT UberHealth App</ion-title>
         </ion-toolbar>
       </ion-header>
-    
-      <div id="container">
-        <strong>Ready to create an app?</strong>
-        <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
-      </div>
+      <ion-grid>
+        <ion-row>
+          <ion-row>
+            <ion-col>
+              <ion-card color="danger">
+                <ion-card-content>
+                  <ion-icon :icon="timer" class="card-icon"></ion-icon>
+                  <ion-text color="light">25 min</ion-text>
+                </ion-card-content>
+              </ion-card>
+            </ion-col>
+          </ion-row>
+        </ion-row>
+        <ion-row>
+          <ion-col>
+            <ion-card>
+              <ion-card-header>
+                <ion-card-title>Activity</ion-card-title>
+              </ion-card-header>
+              <ion-card-content>
+                Founded in 1829 on an isthmus between Lake Monona and Lake Mendota, Madison was named the capital of the Wisconsin Territory in 1836.
+              </ion-card-content>
+            </ion-card>
+          </ion-col>
+          <ion-col>
+            <ion-card>
+              <ion-card-header>
+                <ion-card-title>Activity</ion-card-title>
+              </ion-card-header>
+              <ion-card-content>
+                Founded in 1829 on an isthmus between Lake Monona and Lake Mendota, Madison was named the capital of the Wisconsin Territory in 1836.
+              </ion-card-content>
+            </ion-card>
+          </ion-col>
+        </ion-row>
+      </ion-grid>
     </ion-content>
+    <ion-tabs>
+      <ion-tab-bar slot="bottom">
+        <ion-tab-button tab="activity">
+          <ion-icon :icon="calendar"></ion-icon>
+          <ion-label>Schedule</ion-label>
+          <ion-badge>6</ion-badge>
+        </ion-tab-button>
+        <ion-tab-button tab="account">
+          <ion-icon :icon="bicycle"></ion-icon>
+          <ion-label>Exercise</ion-label>
+        </ion-tab-button>
+        <ion-tab-button tab="account">
+          <ion-icon :icon="barChart"></ion-icon>
+          <ion-label>Ranking</ion-label>
+        </ion-tab-button>
+        <ion-tab-button tab="account">
+          <ion-icon :icon="personCircle"></ion-icon>
+          <ion-label>Account</ion-label>
+        </ion-tab-button>
+      </ion-tab-bar>
+    </ion-tabs>
   </ion-page>
 </template>
 
 <script>
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonTabBar, IonTabButton, IonTabs, IonIcon, IonGrid, IonRow, IonCol } from '@ionic/vue';
+import { IonCard, IonCardContent, IonCardHeader, IonCardTitle, IonLabel, IonBadge } from '@ionic/vue';
+import { IonText } from '@ionic/vue';
 import { defineComponent } from 'vue';
+import { calendar, personCircle, timer, bicycle, diamond, barChart } from 'ionicons/icons';
 
 export default defineComponent({
   name: 'Home',
@@ -32,7 +87,31 @@ export default defineComponent({
     IonHeader,
     IonPage,
     IonTitle,
-    IonToolbar
+    IonToolbar,
+    IonTabs,
+    IonTabButton,
+    IonTabBar,
+    IonIcon,
+    IonGrid,
+    IonCol,
+    IonRow,
+    IonCard,
+    IonCardContent,
+    IonCardHeader,
+    IonCardTitle,
+    IonLabel,
+    IonBadge,
+    IonText,
+  },
+  setup() {
+    return {
+      personCircle,
+      calendar,
+      timer,
+      bicycle,
+      diamond,
+      barChart,
+    }
   }
 });
 </script>
@@ -64,5 +143,13 @@ export default defineComponent({
 
 #container a {
   text-decoration: none;
+}
+
+.card-icon {
+  font-size: 64px;
+}
+
+ion-text {
+  font-size: 64px;
 }
 </style>
