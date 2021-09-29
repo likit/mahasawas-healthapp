42d57b77511f3ff8598f091f1a21bdc6025d61fb
Likit Preeyanon <likit.pre@mahidol.edu>
2021-09-29 16:31:42 +0700

    added walk record form view and route
    moved form from walk record to a new view

---
+++ src/views/Milestone.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "Milestone"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/activities/WalkRecordDetail.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "WalkRecordDetail"
+}
+</script>
+
+<style scoped>
+
+</style>
