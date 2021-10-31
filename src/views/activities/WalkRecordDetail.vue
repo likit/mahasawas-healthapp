bc28abb7db54eff41b82d6e9072b36e34b05055f
Likit Preeyanon <likit.pre@mahidol.edu>
2021-11-01 05:44:49 +0700

    major update
    user can now submit a record to challenges

--- src/views/activities/WalkRecordDetail.vue
+++ src/views/activities/WalkRecordDetail.vue
@@ -21,17 +21,20 @@
                     Distance {{ record.distance }} km
                   </p>
                   <p>
-                    Steps {{ record.steps }}
+                    Estimated Calories {{ record.estimatedCalories }}
                   </p>
                   <p>
-                    ID {{ record.id }}
+                    Calories {{ record.calories }}
+                  </p>
+                  <p>
+                    Steps {{ record.steps }}
                   </p>
                 </ion-label>
               </ion-item>
             </ion-list>
           </ion-col>
         </ion-row>
-        <ion-row>
+        <ion-row v-if="submissions.length == 0">
           <ion-col>
             <ion-button expand="block" @click="deleteRecord" color="danger">
               Delete
@@ -40,30 +43,40 @@
         </ion-row>
         <ion-row>
           <ion-col>
-            <ion-item>
+            <ion-item v-if="submissableChallenges.length > 0">
               <ion-label>
                 โครงการที่เข้าร่วม
               </ion-label>
-              <ion-select v-model="selectedCampaigns" multiple="true">
+              <ion-select v-model="selectedChallenges" multiple="true">
                 <ion-select-option
-                    v-for="camp in campaigns" :key="camp.id" :value="camp">
-                  {{ camp.name}}
+                    v-for="ch in challenges.filter(c=> submissions.indexOf(c.id) < 0 )" :key="ch.id" :value="ch.id">
+                  {{ ch.title}}
                 </ion-select-option>
               </ion-select>
             </ion-item>
-            <ion-list-header>
+            <ion-list-header v-if="submissableChallenges.length > 0">
               <ion-label>ส่งผลไปยังโครงการ</ion-label>
             </ion-list-header>
-            <ion-item v-for="camp in selectedCampaigns" :key="camp.id">
-              <ion-label>{{ camp.name }}</ion-label>
+            <ion-item v-for="ch in selectedChallengeItems" :key="ch.id">
+              <ion-label>{{ ch.title }}</ion-label>
             </ion-item>
+          </ion-col>
+        </ion-row>
+        <ion-row v-if="submissableChallenges.length > 0">
+          <ion-col>
+            <ion-button expand="block" @click="submit" color="primary">
+              Submit
+            </ion-button>
           </ion-col>
         </ion-row>
         <ion-row>
           <ion-col>
-            <ion-button expand="block" @click="deleteRecord" color="primary">
-              Submit
-            </ion-button>
+            <ion-list-header>
+              <ion-label>โครงการที่ส่งผลแล้ว</ion-label>
+            </ion-list-header>
+            <ion-item v-for="ch in challenges.filter(s => submissions.indexOf(s.id) >= 0)" :key="ch.id">
+              <ion-label>{{ ch.title }}</ion-label>
+            </ion-item>
           </ion-col>
         </ion-row>
       </ion-grid>
@@ -90,7 +103,8 @@ import {
 
 import {defineComponent} from 'vue';
 import { db } from '../../firebase'
-import { doc, getDoc, getDocs, collection, deleteDoc } from 'firebase/firestore'
+import {doc, getDoc, getDocs, collection, deleteDoc, query, where, addDoc } from 'firebase/firestore'
+import liff from "@line/liff";
 
 export default defineComponent({
   name: "WalkRecordDetail",
@@ -111,9 +125,26 @@ export default defineComponent({
   },
   data() {
     return {
+      profile: {
+        challenges: [],
+        userId: null
+      },
       record: {},
-      campaigns: [],
-      selectedCampaigns: [],
+      challenges: [],
+      submissions: [],
+      selectedChallenges: [],
+    }
+  },
+  computed: {
+    selectedChallengeItems () {
+      const self = this
+      return this.challenges.filter(ch => {
+        return self.selectedChallenges.indexOf(ch.id) >= 0
+      })
+    },
+    submissableChallenges () {
+      const self = this
+      return this.challenges.filter(ch => self.submissions.indexOf(ch.id) < 0)
     }
   },
   methods: {
@@ -126,6 +157,34 @@ export default defineComponent({
           self.$router.push({ name: 'WalkRecord' })
         })
       }
+    },
+    async submit() {
+      for (const ch of this.selectedChallenges) {
+        let ref = collection(db, 'activity_submissions')
+        addDoc(ref, {
+          recordId: this.$route.params.recordId,
+          userId: this.profile.userId,
+          challengeId: ch,
+          submittedAt: new Date()
+        }).then(async () => {
+          await this.presentSubmit()
+          this.submissions.push(ch)
+        })
+      }
+    },
+    async presentSubmit() {
+      const alert = await alertController
+          .create({
+            cssClass: 'my-custom-class',
+            header: 'Submit Successfully',
+            subHeader: 'ส่งรายการ',
+            message: 'ส่งข้อมูลเรียบร้อยแล้ว',
+            buttons: ['OK'],
+          });
+      await alert.present();
+
+      const { role } = await alert.onDidDismiss();
+      console.log('onDidDismiss resolved with role', role);
     },
     async presentAlert() {
       const alert = await alertController
@@ -144,6 +203,20 @@ export default defineComponent({
   },
   async mounted() {
     const self = this
+    if (liff.isInClient() && liff.isLoggedIn()) {
+      liff.getProfile().then(profile => {
+        self.profile = profile
+      })
+    } else {
+      let ref = collection(db, 'profiles')
+      let q = query(ref, where("userId", "==", "mumthealthtest"))
+      let querySnapshot = await getDocs(q)
+      if (!querySnapshot.empty) {
+        querySnapshot.forEach(d => {
+          self.profile = d.data()
+        })
+      }
+    }
     const recordId = this.$route.params.recordId
     let ref = doc(db, 'activity_records', recordId)
     const docSnapshot = await getDoc(ref)
@@ -151,16 +224,21 @@ export default defineComponent({
       this.record = docSnapshot.data()
       this.record.id = docSnapshot.id
     }
-    let campRef = collection(db, 'campaigns')
-    const documentQuerySnapshot = await getDocs(campRef)
-    documentQuerySnapshot.docs.forEach(d=>{
-      let data = d.data()
-      data.id = d.id
-      if (self.record.submissions &&
-          self.record.submissions.indexOf(data.id) < 0) {
-        self.campaigns.push(data)
-      }
+    ref = collection(db, 'activity_submissions')
+    let q = query(ref, where('userId', '==', self.profile.userId), where('recordId', '==', recordId))
+    let querySnapshot = await getDocs(q)
+    querySnapshot.forEach(s => {
+      self.submissions.push(s.data().challengeId)
     })
+    for (const ch of self.profile.challenges) {
+      let docRef = doc(db, 'challenges', ch)
+      let docSnap = await getDoc(docRef)
+      if (docSnap.exists()) {
+        let data = docSnap.data()
+        data.id = docSnap.id
+        self.challenges.push(data)
+      }
+    }
   }
 })
 </script>
