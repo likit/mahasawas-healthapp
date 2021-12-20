03b79f659977995ac3baaf3dba681988396daaa7
Likit Preeyanon <likit.pre@mahidol.edu>
2021-12-20 09:46:15 +0700

    updated weight view and algorithm

--- src/views/physical/weight.vue
+++ src/views/physical/weight.vue
@@ -6,34 +6,50 @@
           <ion-col>
             <ion-text>
               <div class="ion-text-center">
-                <h1> น้ำหนักตัว </h1>
+                <h1> น้ำหนักตัว</h1>
+                <h4>Body Weight</h4>
               </div>
             </ion-text>
           </ion-col>
         </ion-row>
         <ion-row>
-          <label>{{ new Date().toLocaleDateString() }}</label>
+          <ion-col>
+            <ion-list>
+             <ion-item>
+               <ion-label position="floating">Date</ion-label>
+               <ion-datetime :max="measureDate" display-format="MMM DD, YYYY" v-model="measureDate"></ion-datetime>
+             </ion-item>
+             <ion-item lines="full">
+               <ion-label position="floating">Weight in kilogram</ion-label>
+               <ion-input type="number" placeholder="99.5" v-model="newWeight"></ion-input>
+             </ion-item>
+            </ion-list>
+          </ion-col>
         </ion-row>
         <ion-row>
-         <ion-list>
-           <ion-item>
-             <ion-label> weight </ion-label>
-             <ion-input type="number" placeholder="99.5" v-model="newWeight"></ion-input>
-             <ion-label> kg </ion-label>
-           </ion-item>
-         </ion-list>
+          <ion-col>
+            <ion-button expand="block" color="success" @click="savedata">Save</ion-button>
+          </ion-col>
         </ion-row>
-          <ion-button expand="block" color="success" @click="savedata">Save</ion-button>
+        <ion-row>
+          <ion-col>
+            <h3>Recents</h3>
+          </ion-col>
+        </ion-row>
+        <ion-row>
+          <ion-col>
+            <ion-list>
+              <ion-item v-for="weightrecord in lstWeight.slice(0, 5)" :key="weightrecord.id">
+                <label>Date {{ weightrecord.weightdate.toDate().toLocaleString() }}
+                  <p>
+                    น้ำหนัก {{ weightrecord.weight}} kg
+                  </p>
+                </label>
+              </ion-item>
+            </ion-list>
+          </ion-col>
+        </ion-row>
       </ion-grid>
-      <ion-list>
-        <ion-item v-for="weightrecord in lstWeight.slice(0, 5)" :key="weightrecord.id">
-          <label>Date {{ weightrecord.weightdate.toDate().toLocaleString() }}
-            <p>
-              น้ำหนัก {{ weightrecord.weight}} kg
-            </p>
-          </label>
-        </ion-item>
-      </ion-list>
       <ion-fab vertical="top" horizontal="start" slot="fixed">
         <ion-fab-button @click="$router.push({ name: 'Health' })">
           <ion-icon :icon="arrowBackCircle"></ion-icon>
@@ -54,15 +70,18 @@ import {
   IonLabel,
   IonInput,
   IonFab,
+  IonFabButton,
+  IonIcon,
   IonCol,
   IonText,
   IonButton,
   IonList,
+  IonDatetime,
   IonItem
 } from '@ionic/vue';
 import {db} from "@/firebase";
 import {mapState} from "vuex";
-import {doc, getDocs, orderBy, query, updateDoc, where} from "firebase/firestore";
+import {doc, getDocs, orderBy, query, updateDoc, where, limit} from "firebase/firestore";
 export default {
   name: "weight",
   components :{
@@ -77,14 +96,16 @@ export default {
     IonText,
     IonButton,
     IonList,
-    IonItem
+    IonItem,
+    IonIcon,
+    IonFabButton,
+    IonDatetime
   },
   data(){
     return {
-      DateNow: "",
-      DateN: "",
       newWeight: null,
-      lstWeight: []
+      lstWeight: [],
+      measureDate: new Date().toISOString(),
     }
   },
   setup () {
@@ -93,18 +114,27 @@ export default {
     }
   },
   computed: {
-    ...mapState(['user']),
+    ...mapState(['user', 'currentWeight']),
+    DateN () {
+      let mDate = new Date(this.measureDate)
+      let month = mDate.getMonth() + 1
+      return mDate.getFullYear().toString() + month + mDate.getDate().toString()
+    },
+    isSavable () {
+      return this.newWeight != null
+    }
   },
   watch:{
     DateN: async function (newValue,oldValue) {
-       this.loadWeight(newValue,oldValue)
+       await this.loadWeight(newValue, oldValue)
     }
   },
   methods:{
     async loadWeight(){
+      this.lstWeight = []
       let ref = collection(db, 'weight')
-      let q = query(ref, where("userId", "==", this.$store.state.user.userId),
-          orderBy('weightdate','desc'))
+      let q = query(ref, where("userId", "==", this.user.userId),
+          orderBy('weightdate','desc'), limit(5))
       let querySnapshot = await getDocs(q)
       const self = this
       querySnapshot.forEach(d => {
@@ -113,29 +143,27 @@ export default {
         self.lstWeight.push(data)
       })
     },
-    async savedata(){
+    async savedata() {
       const ref = collection(db, 'weight')
-      let q = query(ref, where("userId", "==", this.$store.state.user.userId), where("datetxt", "==",this.DateN),
+      let q = query(ref, where("userId", "==", this.user.userId),
+          where("datetxt", "==",this.DateN),
           orderBy('weightdate','desc'))
       let querySnap = await getDocs(q)
       if (querySnap.empty) {
         let data ={
-          userId: this.$store.state.user.userId,
-          weightdate: Timestamp.fromDate(new Date()),
+          userId: this.user.userId,
+          weightdate: Timestamp.fromDate(new Date(this.measureDate)),
           datetxt: this.DateN,
           weight: this.newWeight
         }
         addDoc(ref, data).then((docRef)=> {
           data.id = docRef.id
           this.$store.dispatch('updateWeight',this.newWeight)
-          console.log(this.newWeight)
-        })}else {
+        })
+      } else {
         querySnap.forEach(d => {
           let docRef = doc(db, 'weight', d.id)
           let updateWeight = {
-            userId: this.$store.state.user.userId,
-            weightdate: Timestamp.fromDate(new Date()),
-            datetxt: this.DateN,
             weight: this.newWeight
           }
           updateDoc(docRef, updateWeight).then(() => {
@@ -143,17 +171,12 @@ export default {
           })
         })
       }
-      this.lstWeight = []
-      this.loadWeight()
-      }
-
+      await this.loadWeight()
+    }
   },
   mounted() {
-    let strDateY = new Date().getFullYear()
-    let strDateM = new Date().getMonth()
-    let strDateD = new Date().getDate()
-    this.DateN = strDateY.toString() + strDateM.toString() + strDateD.toString()
-    this.newWeight = this.$store.state.currentWeight
+    this.newWeight = this.currentWeight
+    this.loadWeight()
   }
 }
 </script>
